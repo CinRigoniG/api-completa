a1cc131e11dd19efb141c096b68e8d07a95aaad8
CinRigoniG <rigonicinthia@gmail.com>
2024-06-19 08:21:48 -0300

    Ultimos cambios

--- src/main/java/com/example/miPrimeraApi/controllers/ClienteController.java
+++ src/main/java/com/example/miPrimeraApi/controllers/ClienteController.java
@@ -1,8 +1,10 @@
 package com.example.miPrimeraApi.controllers;
 
 import com.example.miPrimeraApi.entities.Cliente;
+import com.example.miPrimeraApi.entities.Domicilio;
 import com.example.miPrimeraApi.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,8 +13,17 @@ import java.util.List;
 @RequestMapping("/clientes")
 public class ClienteController extends BaseController<Cliente, Long>{
 
+    @Autowired
+    private ClienteService clienteService;
+
     public ClienteController(ClienteService clienteService){
         super(clienteService);
+    }
+
+    @GetMapping("/domicilios/{id}")
+    public ResponseEntity<List<Domicilio>> listarDomiciliosPorCliente(@PathVariable Long id) throws Exception {
+        List<Domicilio> domicilios = clienteService.listarDomiciliosPorCliente(id);
+        return ResponseEntity.ok(domicilios);
     }
 
 }
--- src/main/java/com/example/miPrimeraApi/services/ClienteService.java
+++ src/main/java/com/example/miPrimeraApi/services/ClienteService.java
@@ -1,16 +1,38 @@
 package com.example.miPrimeraApi.services;
 
 import com.example.miPrimeraApi.entities.Cliente;
+import com.example.miPrimeraApi.entities.Domicilio;
 import com.example.miPrimeraApi.repositories.BaseRepository;
 import com.example.miPrimeraApi.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ClienteService extends BaseService<Cliente, Long> {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     public ClienteService(ClienteRepository clienteRepository){
         super(clienteRepository);
+    }
+
+    @Transactional
+    public List<Domicilio> listarDomiciliosPorCliente(Long idCliente) throws Exception {
+        try{
+            List<Domicilio> domicilios = new ArrayList<>();
+            Cliente cliente = clienteRepository.findById(idCliente).orElse(null);
+            if(cliente != null){
+                domicilios = new ArrayList<>(cliente.getDomicilios());
+            }
+            return domicilios;
+        }catch (Exception ex){
+            throw new Exception(ex.getMessage());
+        }
     }
 
 }
